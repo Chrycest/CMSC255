3ba96f87a4c967583aedef121fe4714946217331
Chrycest <57018408+Chrycest@users.noreply.github.com>
2022-08-24 11:29:45 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/**************************************************
+* Java Program Name
+***************************************************
+* Christian Ward
+* 8/24/2022
+* CMSC 255 CRN 37573
+***************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Christian");
+		System.out.println("Hello");
+		System.out.println("Output of text");
+		System.out.println("8/24/2022");
+		System.out.println("CMSC 255 CRN 37573");
+		System.out.println();
+	}
+}
